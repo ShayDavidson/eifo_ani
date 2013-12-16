73dc1dcf5739fa32d1d38576c97c739052d184b4
Shay Davidson <shay.h.davidson@gmail.com>
2013-12-16 20:57:40 +0200

    Initial commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EifoAni::Application.config.secret_key_base = '6602d8f6a5f7a7d2dc7f8503bb32ede856240018320129fa0845dd438bdf254839598d1af3a34beaa697c78be704a807f9661b0401a4840b046834fa1206f6db'
